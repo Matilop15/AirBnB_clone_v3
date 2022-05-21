f9f6de5eb6fa841f59ec16bf2c462cdf10088022
Matilop15 <3959@holbertonschool.com>
2022-05-21 01:45:11 +0000

    update task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,8 +4,8 @@ Create the API
 """
 from os import getenv
 from flask import Flask, Blueprint
-from models import storage
 from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,4 +18,4 @@ def teardown(self):
 if __name__ == '__main__':
     api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
     api_port = getenv('HBNB_API_PORT', default=5000)
-    app.run(host=api_host, port=int(api_port), threaded=True)
+    app.run(debug=True, host=api_host, port=int(api_port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,9 @@
 I don´tknow
 """
 from api.v1.views import app_views
-
+from flask import jsonify
 
 @app_views.route("/status", strict_slashes=False)
 def status():
-	"""Return ok"""
-	return {'status': 'OK'}
+    """Return ok"""
+    return jsonify({'status': 'OK'})
