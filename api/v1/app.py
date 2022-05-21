95889dfe28a06bdbdb8244588191848e2d3ca255
Marcosozhol <3956@holbertonschool.com>
2022-05-21 00:28:27 -0300

    Task 6 - error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Create the API
 """
 from os import getenv
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 from models import storage
 
@@ -14,8 +14,12 @@ app.register_blueprint(app_views)
 def teardown(self):
     """Closes storage session"""
     storage.close()
+    
+def not_found(error):
+    return jsonify("error": "Not found")
 
 if __name__ == '__main__':
     api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
     api_port = getenv('HBNB_API_PORT', default=5000)
     app.run(debug=True, host=api_host, port=int(api_port), threaded=True)
+    app.register_error_handler(404, not_found)
