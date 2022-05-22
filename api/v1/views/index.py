c6f0dbeee140ff6e08f62deb0c43ff2605fdfc6b
Marcosozhol <3956@holbertonschool.com>
2022-05-22 01:08:31 -0300

    Correction code to format

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,9 +21,9 @@ def status():
 @app_views.route("/stats")
 def stats():
     """Returns the number of each objects by type"""
-    return jsonify({"amenities": storage.count(Amenity),
-                    "cities": storage.count(City),
-                    "places": storage.count(Place),
-                    "reviews": storage.count(Review),
-                    "states": storage.count(State),
-                    "users": storage.count(User)})
+    return jsonify(amenities=storage.count(Amenity),
+                    cities=storage.count(City),
+                    places=storage.count(Place),
+                    reviews=storage.count(Review),
+                    states=storage.count(State),
+                    users=storage.count(User))
