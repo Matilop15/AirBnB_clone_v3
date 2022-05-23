ed398735b571b135953df25266af51ad97235cd3
Marcosozhol <3956@holbertonschool.com>
2022-05-23 00:03:07 -0300

    fixed something

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,7 +13,7 @@ def all_states():
     """Retrieves the list of all State objects: GET /api/v1/states"""
     all_states = storage.all(State)
     list_states = []
-    for state in all_states:
+    for state in all_states.values():
         list_states.append(state.to_dict())
 
     return jsonify(list_states)
