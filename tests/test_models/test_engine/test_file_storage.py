16b9240ecc0f0d4c6f71f81dd2f61d948eef3a79
Marcosozhol <3956@holbertonschool.com>
2022-05-23 18:44:00 -0300

    add tests in test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -165,3 +165,22 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(city_count, len(storage.all(City)))
         self.assertEqual(place_count, len(storage.all(Place)))
         self.assertEqual(all_count, len(storage.all()))
+        
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
+        storage = FileStorage()
+        new_dict = {}
+        for key, value in classes.items():
+            instance = value()
+            instance_key = instance.__class__.__name__ + "." + instance.id
+            new_dict[instance_key] = instance
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = new_dict
+        storage.save()
+        FileStorage._FileStorage__objects = save
+        for key, value in new_dict.items():
+            new_dict[key] = value.to_dict()
+        string = json.dumps(new_dict)
+        with open("file.json", "r") as f:
+            js = f.read()
+        self.assertEqual(json.loads(string), json.loads(js))
