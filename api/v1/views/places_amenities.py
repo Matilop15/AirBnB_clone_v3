89f3875d77878f4cc4a2b1b34ae17ec88549be08
Matilop15 <3959@holbertonschool.com>
2022-05-24 00:49:09 +0000

    probandooo

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -24,13 +24,13 @@ def get_place_amenities(place_id=None):
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        list_places_amenities = place.amenities
+    else:
         amens = storage.all(Amenity)
         for amenity in amens.values():
             if amenity.id in place.amenity_ids:
                 list_places_amenities.append(amenity)
-    else:
-        list_places_amenities = place.amenities
 
     return jsonify([amenity.to_dict() for amenity in list_places_amenities])
 
@@ -49,17 +49,7 @@ def delete_place_amenity(place_id, amenity_id):
     if not amenity:
         abort(404)
 
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
-        if amenity not in place.amenity_ids:
-            abort(404)
-        index = None
-        for idx, id in enumerate(place.amenity_ids):
-            if amenity.id == id:
-                index = idx
-                break
-        del place.amenity_ids[index]
-        place.save()
-    else:
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
         index = None
         for idx, amen in enumerate(place.amenities):
             if amen.id == amenity.id:
@@ -68,6 +58,16 @@ def delete_place_amenity(place_id, amenity_id):
             if index is None:
                 abort(404)
         del place.amenities[index]
+        place.save()
+    else:
+        if amenity not in place.amenity_ids:
+            abort(404)
+        index = None
+        for idx, id in enumerate(place.amenity_ids):
+            if amenity.id == id:
+                index = idx
+                break
+        del place.amenity_ids[index]
         place.save()
     return jsonify({}), 200
 
@@ -86,17 +86,18 @@ def post_place_amenity(place_id, amenity_id):
     if not amenity:
         abort(404)
 
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        if amenity in place.amenities:
+            return jsonify(amenity.to_dict()), 200
+        else:
+            place.amenities.append(amenity)
+            place.save()
+    else:
         if amenity_id in place.amenity_ids:
             return jsonify(amenity.to_dict()), 200
         else:
             place.amenity_ids.append(amenity_id)
             place.save()
             return jsonify(amenity.to_dict()), 201
-    else:
-        if amenity in place.amenities:
-            return jsonify(amenity.to_dict()), 200
-        else:
-            place.amenities.append(amenity)
-            place.save()
-            return jsonify(amenity.to_dict()), 201
+
+    return jsonify(amenity.to_dict()), 201
